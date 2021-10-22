16951e3011622320222dfe1abd58f88a9b77e09d
Sandro <valksandro@gmail.com>
2021-10-22 16:48:12 -0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Valdiney Menezes on 22/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
